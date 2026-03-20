0015cd9f2e0be9ba797625fcf41f942554154e4d
Pratik5545 <prateektripathi5545@gmail.com>
2026-03-21 00:22:48 +0530

    update cors and api urls for production

--- orchestrator/Program.cs
+++ orchestrator/Program.cs
@@ -35,7 +35,9 @@ builder.Services.AddCors(options =>
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://kernel-flow-dzli9k0ne-pratik5545s-projects.vercel.app"
+                
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
